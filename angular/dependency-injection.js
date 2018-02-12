d7dffe00fa55843830ebc57931851dd75f827aca
Navneet Prakash <hello_navneet@hotmail.com>
2018-02-12 10:55:33 +0530

    Inject an angularjs service in the console.

--- angular/dependency-injection.js
+++ angular/dependency-injection.js
@@ -8,7 +8,10 @@ function logit(person) {
 }
 
 // object is created out side the function. Here new person object sumit is dependent on person object.
-var sumit = new person('sumit', 'singh'); 
-
+var sumit = new person('sumit', 'singh');
 
 logit(sumit);
+
+
+/* HOW TO INJECT A SERVICE IN THE CONSOLE TO DEBUG THE APPLICATION BY PLAYING WITH SERVICE */
+var service = angular.element(document.body).injector().get('serviceName');
