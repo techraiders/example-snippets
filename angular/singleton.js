1db02ac7300f5bd452e462e268a38f9bb9b43008
Navneet Prakash <hello_navneet@hotmail.com>
2018-02-12 17:29:47 +0530

    Implements singleton design pattern using js

--- angular/singleton.js
+++ angular/singleton.js
@@ -8,3 +8,30 @@ myApp.controller('secondController', ['$scope', '$log', function($scope, $log) {
   $log.name = 'I am second';
 }]);
 // Here both names will be added into only one original $log because $log is a service whoose new instance can't be created.
+
+
+
+/* IMPLEMENTING SINGLETON DESIGN PATTERN IN VANILLA JAVASCRIPT */
+
+var Singleton = (function () {
+  var instance;
+  
+  function createInstance () {
+    var object = new Object('I am the instance');
+    return object;
+  }
+  
+  return {
+    getInstance : function () {
+      if (!instance) instance = createInstance();
+      return instance;
+    }
+  };  
+})();
+
+function run() {
+  var instance1 = Singleton.getInstance();
+  var instance2 = Singleton.getInstance();
+  
+  alert('Same instance? ' + (instance1 === instance2));
+}
