fe90b97f1c0d0eba5d3839cba6c2ac5c967a31ee
Navneet Prakash <hello_navneet@hotmail.com>
2018-02-15 12:43:51 +0530

    Update console.log.js

--- angular/console.log.js
+++ angular/console.log.js
@@ -4,3 +4,7 @@ app.config(['$logProvider', function() {
 
 // this disables debug-lever messages.
 // Prefer $log over console.log($log is safe and handles case when the browser does not implement console logging.)
+
+
+/* HOW TO INJECT A SERVICE IN THE CONSOLE TO DEBUG THE APPLICATION BY PLAYING WITH SERVICE */
+var service = angular.element(document.body).injector().get('serviceName');
