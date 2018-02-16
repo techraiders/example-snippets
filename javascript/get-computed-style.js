d3e825c8cec5a1c736a3e06b288e62f1bb65fcdb
Navneet Prakash <hello_navneet@hotmail.com>
2018-02-16 12:27:45 +0530

    adds css rule to stylesheet using js

--- javascript/get-computed-style.js
+++ javascript/get-computed-style.js
@@ -13,3 +13,19 @@
 );
 
 //var y = document.defaultView.getComputedStyle($0, null).getPropertyValue('z-index');
+
+
+/* ADD and REMOVE RULES DIRECTLY TO STYLESHEETS:
+We're all well versed in modifying styles via the element.style.propertyName method, and we've used JavaScript toolskits to do it, but did you know you can actually read and write rules within new and existing stylesheets?
+*/
+
+function addCSSRule (sheet, selector, rules, index) {
+  if (sheet.insertRule) {
+    sheet.insertRule(selector + "{" + rules + "}", index);
+  } else {
+    sheet.addRule(selector, rules, index);
+  }
+}
+
+Use it!
+  CSSRule(document.stylesheets[0], 'header', 'float: left');
