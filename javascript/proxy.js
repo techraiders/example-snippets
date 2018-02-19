0950d5dd3120bb1c6efe2b55bef6e3f48ebfba95
Navneet Prakash <hello_navneet@hotmail.com>
2018-02-19 21:20:32 +0530

    debugs insertion into js array using js proxy
    Finds the culprit code statement or function (if any) pushing an item into your JavaScript array, using JavaScript Proxy.

--- javascript/proxy.js
+++ javascript/proxy.js
@@ -16,3 +16,25 @@ const wrapped = wrap(object);
 
 console.log(wrapped.message); // "Reading property message"
 // "Hello world"
+
+
+
+
+/* FINDING CULPRIT INSERTING AN ITEM INTO YOUR JAVASCRIPT ARRAY */
+
+function proxyArray (arr) {
+  var handler = {
+    set: function (target, property, value, receiver) {
+      debugger;
+      target[property] = value;      
+      return value;
+    }
+  };
+  return new Proxy(arr, handler);
+}
+
+var arr = [1,2,3,4],
+    arr = proxyArray(arr);
+
+arr.push(5);
+console.log(arr);
