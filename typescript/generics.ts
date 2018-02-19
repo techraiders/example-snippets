9989035f9af4e3387f32c5f1030943aa1204af7a
Navneet Prakash <hello_navneet@hotmail.com>
2018-02-19 23:21:38 +0530

    adds and uses KeyValuePairPrinter generic class.

--- typescript/generics.ts
+++ typescript/generics.ts
@@ -24,3 +24,17 @@ class KeyValuePair<TKey, TValue> {
 let pair1 = new KeyValuePair<number, string>(1, 'First');
 let pair2 = new KeyValuePair<string, Date>('Second', new Date(Date.now());
 let pair3 = new KeyValuePair<number, string>(3, 'Third');
+
+
+class KeyValuePairPrinter<T, U> {
+  constructor(private pairs: KeyValuePair<T, U>[]) {}
+  
+  print() {
+    for (let p of this.pairs) {
+      console.log(`${p.key}: ${p.value}`);
+    }
+  }
+}
+
+var printer = new KeyValuePairPrinter([pair1, pair3]);
+printer.print();
