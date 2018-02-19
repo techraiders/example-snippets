ec9e18d3d00650fb3e8aea30d0aa09b7665dcf3a
Navneet Prakash <hello_navneet@hotmail.com>
2018-02-19 23:11:53 +0530

    Create generics.ts
    Demonstrates some basic use cases of generics in typescript.

---
+++ typescript/generics.ts
@@ -0,0 +1,26 @@
+function clone<T>(obj: T): T   {
+  let serialized = JSON.stringify(obj);
+  return JSON.parse(obj);
+}
+
+clone('Hello!');
+clone(123);
+
+var todo: Todo = {
+  id: 1, name: 'Pick up drycleaning', state: TodoState.Active
+};
+clone(todo);
+
+
+var array: number[] = [1,2,3],
+    array2: Array<number> = [1,2,3];
+
+
+
+class KeyValuePair<TKey, TValue> {
+  constructor(public key: TKey, public value: TValue) {};
+}
+
+let pair1 = new KeyValuePair<number, string>(1, 'First');
+let pair2 = new KeyValuePair<string, Date>('Second', new Date(Date.now());
+let pair3 = new KeyValuePair<number, string>(3, 'Third');
