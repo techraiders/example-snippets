2de703db452531a1d61cbcdb043d45d9a15a173c
Navneet Prakash <hello_navneet@hotmail.com>
2018-02-20 12:46:14 +0530

    Validates object's property set using Js proxies

--- javascript/proxy.js
+++ javascript/proxy.js
@@ -45,3 +45,33 @@ var arr = [1,2,3,4],
 
 arr.push(5);
 console.log(arr);
+
+
+
+
+// USING PROXIES TO ENFORCE VALUE VALIDATION
+var validator = {
+  set: function (obj, prop, value) {
+    if (prop === 'age') {
+      if(!Number.isInteger(value)) {
+        throw new TypeError('The age is not an integer');
+      }
+      if (value > 200) {
+        throw new RangeError('The age seems invalid');
+      }
+    }
+    
+    // The default behavior to store the value
+    obj[prop] = value;
+    
+    // Indicate success
+    return true;
+  }
+};
+
+var person = new Proxy({}, validator);
+    person.age = 100;
+    console.log(person.age); // 100
+
+    person.age = 'young'; // Throws an exception - The age is not an integer.
+    person.age = 300; // Throws an exception - The age is invalid.
