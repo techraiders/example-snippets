a5718f233d005743dc1d80aa546987ba2ad5a3b6
Navneet Prakash <hello_navneet@hotmail.com>
2018-04-27 16:15:03 +0530

    Create toPrimitive.js

---
+++ javascript/toPrimitive.js
@@ -0,0 +1,25 @@
+// Syntax:
+
+obj[Symbol.toPrimitive] = function(hint) {
+  // return a primitive value
+  // hint = one of "string", "number", "default"
+}
+
+
+
+// Example:
+
+let user = {
+  name: "John",
+  money: 1000,
+
+  [Symbol.toPrimitive](hint) {
+    alert(`hint: ${hint}`);
+    return hint == "string" ? `{name: "${this.name}"}` : this.money;
+  }
+};
+
+// conversions demo:
+alert(user); // hint: string -> {name: "John"}
+alert(+user); // hint: number -> 1000
+alert(user + 500); // hint: default -> 1500
