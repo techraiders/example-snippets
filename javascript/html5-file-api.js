d22789ac67ddac2b89bf02c3ff8342ff930ed27e
Navneet Prakash <hello_navneet@hotmail.com>
2018-06-16 09:37:01 +0530

    Create html5-file-api.js

---
+++ javascript/html5-file-api.js
@@ -0,0 +1,9 @@
+/*
+What you can do with the File API:
+  1. Perform client-side file operations, such as limiting the size of uploads.
+  2. Restricts the kinds of files that can be uploaded to a server.
+  3. Generate thumbnail versions of images that are being uploaded.
+  4. Check to see if the modification date of a local file is before or after a certain date.
+  5. Parse the content of files locally to get detailed information. (e.g Spell check for a text file)
+  6. Modify file contents before they are sent off to the server.
+*/
