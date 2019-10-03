a4731c8d0c3761aaa426c09f41bb8cc1a3bf3e57
Navneet Prakash <hello_navneet@hotmail.com>
2019-10-03 16:17:57 +0530

    Update util.js

--- javascript/util.js
+++ javascript/util.js
@@ -1,8 +1,8 @@
-FIND SIMILAR OR COMMON:
+// FIND SIMILAR OR COMMON:
 const similarity = (arr, values) => arr.filter(v => values.includes(v));
 similarity([1, 2, 3], [1, 2, 4]); // [1, 2]
 
-SMOOTH SCROLL:
+// SMOOTH SCROLL:
 This snippet can be used to smoothly scroll the element on which it is called into the visible area of the browser window.
 const smoothScroll = element =>
   document.querySelector(element).scrollIntoView({
@@ -13,7 +13,7 @@ smoothScroll('#fooBar'); // scrolls smoothly to the element with the id fooBar
 smoothScroll('.fooBar'); // scrolls smoothly to the first element with a class of fooBar
 
 
-SORT CHARACTERS IN STRING
+// SORT CHARACTERS IN STRING
 This snippet can be used to alphabetically sort the characters in a string.
 
 const sortCharactersInString = str => [...str].sort((a, b) => a.localeCompare(b)).join('');
