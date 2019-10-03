35e2980205a1b12ee773ada3021ed2736e3a46a7
Navneet Prakash <hello_navneet@hotmail.com>
2019-10-03 18:51:19 +0530

    Update util.js

--- javascript/util.js
+++ javascript/util.js
@@ -29,3 +29,21 @@ first.toLowerCase() + rest.join('')
 
 decapitalize('FooBar'); // 'fooBar'
 decapitalize('FooBar'); // 'fooBar'
+
+// DEEP FLATTEN
+const deepFlatten = arr => [].concat(...arr.map(v => (Array.isArray(v) ? deepFlatten(v) : v)));
+deepFlatten([1, [2], [[3], 4], 5]); // [1,2,3,4,5]
+
+// DEFER
+// This snippet delays the execution of a function until the current call stack is cleared.
+const defer = (fn, ...args) => setTimeout(fn, 1, ...args);
+defer(console.log, 'a'), console.log('b'); // logs 'b' then 'a'
+
+// DIFFERENCE
+// This snippet finds the difference between two arrays.
+const difference = (a, b) => {
+  const s = new Set(b);
+  return a.filter(x => !s.has(x));
+};
+
+difference([1, 2, 3], [1, 2, 4]); // [3]
